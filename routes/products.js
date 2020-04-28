f0445b4e063e21dddac7f63eb158ccdddec1e80a
ghogus <claudio.omar.jr@gmail.com>
2020-04-28 00:48:44 -0300

    alterações para update na qty do cart

--- routes/products.js
+++ routes/products.js
@@ -76,75 +76,44 @@ module.exports = () => {
     router.post('/cart', (req, res, next) => {
         log('client/cart...')
         let registrationDate = new Date()
-
-        // let add = req.body.add
-        // let remove = req.body.remove
-        // let product = null
-
+        let user = req.user
+        console.log('user: ' + user)
         let sku = null
         let add = false
         let rem = false
-
         if (req.body.add !== undefined) {
             sku = req.body.add
-            console.log('1) ADD: ' + req.body.add)
             add = true
-            console.log('1) ADD: ' + add)
         }
         else if (req.body.remove != undefined) {
             sku = req.body.remove
-            console.log('2) REM: ' + req.body.remove)
             rem = true
-            console.log('2) REM: ' + rem)
         }
-        console.log('3) SKU: ' + sku)
-
-        // criar function addProductToCart...
-
-        console.log('token 1: ', req.cookies.sessionId)
-        // if (req.cookies.sessionId !== undefined) {
-        // res.cookies.clearCookie('sessionId')
-        // console.log('token 1: ', req.cookies.sessionId)
-        // } else {
-        //     console.log('token 3: ', req.cookies.sessionId)
-        // }
-
-        // testar aqui se o cookie "session-id" já existe
+        // verifica se já existe o cookie "session-id" (que contém o token), caso não, cria um novo token e cart
         if (req.cookies.sessionId === undefined) {
             console.log('Não existe token...')
-            // obtem um novo token, gera o cart salva o token (no cookie e no cart), add o product ao cart
+            //gera um novo token
             log('client/cart/session-id...')
             request.get(process.env.API_GATEWAY + '/session-id', (error, result) => {
                 if (error) { return console.log('ERROR: ' + error) }
-                // console.log('CODE: ' + result.statusCode)
-
-                // cria o cart, salva o token gerado (no cookie e no cart), add o product ao cart e direciona ao cart
-
                 if (result.statusCode == 200) {
-                    res.cookie('sessionId', JSON.parse(result.body).token, { maxAge: 20000 })
-
+                    // add o token ao cookie e define a vida últil dele: 60mim (teste)
+                    let ageSessionId = 1000*60*5
+                    res.cookie('sessionId', JSON.parse(result.body).token, { maxAge: ageSessionId })
                     let sessionId = JSON.parse(result.body).token
                     let authenticated = req.isAuthenticated()
-
+                    // busca o produto para add ao cart, através do sku vindo da PDP
                     let findProduct = { values: { sku: sku }, fields: 'sku', ordination: 1, limit: 1 }
                     log('client/cart/product...')
                     request.get(API_GATEWAY + '/product/' + JSON.stringify(findProduct), (error, result) => {
                         if (error) { return console.log('ERROR: ' + error) }
-                            // console.log('CODE: ' + result.statusCode)
-                            // console.log('\nRESULT: ' + result.body)
-                            // console.log('SIZE: ' + JSON.parse(result.body).length)
-                            if (result.statusCode == 200) {
+                        if (result.statusCode == 200) {
                             let product = null
                             if (JSON.parse(result.body).length > 0) {
                                 product = JSON.parse(result.body)[0]
-
-                                console.log('PRODUCT: ' + product.sku)
-
-                                // let ObjectId = require('mongodb').ObjectId;
                                 let productId = mongoose.Types.ObjectId(product._id);
-                                let imageId = mongoose.Types.ObjectId(product.images[0]._id);
-
                                 if (add) {
+                                    // cria um novo cart, add produto e token ao cart
                                     log('client/cart/post...')
                                     request.post(API_GATEWAY + '/cart', {
                                         json: {
@@ -167,49 +136,33 @@ module.exports = () => {
                                             'isGift': false,
                                             'voucher': null,
                                             'postalCode': null,
-                                            'customer': req.user,
+                                            'customer': user,
                                             'registrationDate': registrationDate.toLocaleString(),
                                             'changeDate': registrationDate.toLocaleString()
                                         }
                                     }, (error, response, body) => {
                                         if (error) { return console.log('ERRO: ' + error) }
-                                        // console.log('\nCODE: ' + response.statusCode)
-                                        // console.log('\nMESSAGE: ' + body.message + '\n')
                                         if (response.statusCode == 200) {
+                                            // busca o cart recém criado e exibe a página do cart
                                             let findCart = { values: { sessionId: sessionId }, fields: 'sessionId', ordination: 1, limit: 1 }
-                                            
                                             log('client/cart/search...')
                                             request.get(API_GATEWAY + '/cart/' + JSON.stringify(findCart), (error, result) => {
                                                 if (error) { return console.log('ERROR: ' + error) }
-                                                // console.log('\nCODE: ' + result.statusCode)
-                                                // console.log('\nRESULT: ' + result.body)
                                                 if (result.statusCode == 200) {
                                                     let cart = JSON.parse(result.body)[0]
-
-                                                    console.log('PRODUCTS: ' + cart.products[0])
-
-                                                    cart.products.forEach(function (product) {
-                                                        console.log('SKU: ' + product.sku)
-                                                        console.log('PRICE: ' + product.price)
-                                                        console.log('DISCOUNT: ' + product.discount)
-                                                    })
-
                                                     if (req.isAuthenticated()) {
                                                         return res.render('index', {
                                                             page: './templates/cart',
                                                             title: APP_TITLE,
                                                             menu: 'full',
-                                                            cart: cart,
-                                                            products: cart.products,
-                                                            message: null
+                                                            cart: cart
                                                         })
                                                     }
                                                     return res.render('index', {
                                                         page: './templates/cart',
                                                         title: APP_TITLE,
                                                         menu: 'small',
-                                                        cart: cart,
-                                                        message: null
+                                                        cart: cart
                                                     })
                                                 }
                                             })
@@ -223,49 +176,105 @@ module.exports = () => {
             })
         } else {
 
-            console.log('Já existe token...')
+            // verifica se já existe o cookie "session-id" (que contém o token), caso não, cria um novo token e cart
             // recupera o token, consulta o cart pelo token, add o product ao cart e direciona ao cart
 
-            // const product = getProduct(sku)
-            // product.then(product => console.log('\nSKU: ' + product.sku + '\n'))
-            // const product = getProduct2(sku)
-            // console.log('sku: ' + product.sku)
+            console.log('Já existe token...')
 
-            // console.log('Aqui...')
-
-            // log('getProduct(' + sku + ')...')
-
-            // console.log('ID: ' + req.params.id)
-            let search = { values: { sku: sku }, fields: 'sku', ordination: 1, limit: 1 }
-            request.get(API_GATEWAY + '/product/' + JSON.stringify(search), (error, result) => {
+            // busca o produto para add/update(qrt)/delete ao cart, através do sku vindo da PDP
+            let findProduct = { values: { sku: sku }, fields: 'sku', ordination: 1, limit: 1 }
+            log('client/cart/product...')
+            request.get(API_GATEWAY + '/product/' + JSON.stringify(findProduct), (error, result) => {
                 if (error) { return console.log('ERROR: ' + error) }
-                // console.log('CODE: ' + result.statusCode)
                 if (result.statusCode == 200) {
-                    // console.log('\nRESULT: ' + result.body)
-                    // console.log('SIZE: ' + JSON.parse(result.body).length)
                     let product = null
                     if (JSON.parse(result.body).length > 0) {
                         product = JSON.parse(result.body)[0]
-                        // console.log('PRODUCT.SKU: ' + product.sku)
-                    }
-                    if (req.isAuthenticated()) {
-                        return res.render('index', {
-                            page: './templates/cart',
-                            title: APP_TITLE,
-                            menu: 'full',
-                            product: product,
-                            message: null
+                        let productId = mongoose.Types.ObjectId(product._id);
+                        // busca o cart recém criado e exibe a página do cart
+                        let sessionId = req.cookies.sessionId
+                        let findCart = { values: { sessionId: sessionId }, fields: 'sessionId', ordination: 1, limit: 1 }
+                        log('client/cart/search...')
+                        request.get(API_GATEWAY + '/cart/' + JSON.stringify(findCart), (error, result) => {
+                            if (error) { return console.log('ERROR: ' + error) }
+                            if (result.statusCode == 200) {
+                                let cart = JSON.parse(result.body)[0]
+                                let products = cart.products
+                                console.log('SIZE: ' + products.length)
+                                if (products.length > 0) {
+                                    let isEquals = false
+                                    let count = 0
+                                    let position = 0
+                                    let compare = null
+                                    let product = null
+                                    while (count < products.length) {
+                                        compare = products[position].sku.localeCompare(sku)
+                                        if (compare == 0 && !isEquals) {
+                                            isEquals = true
+                                            product = products[count]
+                                            position = count
+                                            cart.products[position].qty++
+                                        }
+                                        console.log('SKU: ' + sku + ' | P.SKU: ' + products[count].sku + ' | EQUAL: ' + compare)
+                                        count++                                        
+                                    }
+
+                                    if (!isEquals) {
+                                        console.log('isExist: ' + isEquals)
+
+                                        // add item cart (NOVO ITEM)
+
+                                    } else {
+                                        console.log('product: ' + JSON.stringify(product))
+
+                                        // cart.products[position].qty++
+
+                                        console.log('QTY: ' + products[position].qty)
+                                        console.log('cart: ' + JSON.stringify(cart))
+                                        // cart.changeDate = new Date().toLocaleString()
+
+                                        request.patch(API_GATEWAY + '/cart/' + cart._id + '_' + products[position]._id, {
+                                            json: {
+                                                'products.$.qty': products[position].qty,
+                                                'changeDate': new Date().toLocaleString()
+                                            }
+                                        }, (error, response, body) => {
+                                            if (error) { return console.log('ERRO: ' + error) }
+                                            if (response.statusCode == 200) {
+                                                if (req.isAuthenticated()) {
+                                                    return res.render('index', {
+                                                        page: './templates/cart',
+                                                        title: APP_TITLE,
+                                                        menu: 'full',
+                                                        cart: cart
+                                                    })
+                                                }
+                                                return res.render('index', {
+                                                    page: './templates/cart',
+                                                    title: APP_TITLE,
+                                                    menu: 'small',
+                                                    cart: cart
+                                                })
+                                            }
+                                        })
+                                    }
+                                    // if (compare == 0) {
+                                    //     console.log('produto já existe... Position: ' + position)
+                                    // } else {
+                                    //     console.log('produto não existe... Position: ' + position)
+                                    // }
+                                    // if (add) {
+                                    //     console.log('Aqui... Add...')
+                                    //     // add produto ao cart (add quando novo item ou update qty de item já add anteriormente)
+                                    // } else if (rem) {
+                                    //     console.log('Aqui... Rem...')
+                                    //     // remove produto do cart
+                                    // }
+                                }
+                            }
                         })
                     }
-                    return res.render('index', {
-                        page: './templates/cart',
-                        title: APP_TITLE,
-                        menu: 'small',
-                        product: product,
-                        message: null
-                    })
                 }
-                problem(res, result.statusCode)
             })
         }
     })
@@ -364,33 +373,7 @@ module.exports = () => {
             saleable = true
         let onlineDate = new Date(req.body.onlineDate)
         let saleableDte = new Date(req.body.saleableDate)
-        let images = null
-
         // console.log('VALOR: ' + (parseFloat(price) - (((parseFloat(price) / 100) * discount))).toFixed(2).replace('.', ',').split('').reverse().map((v, i) => i > 5 && (i + 6) % 3 === 0 ? `${v}.` : v).reverse().join('') )
-
-        // log('client/images/post...')
-        // request.post(API_GATEWAY + '/images?token=' + req.session.token, {
-        //     json: {
-        //         'name': req.body.image,
-        //         'registrationDate': registrationDate.toLocaleString(),
-        //         'changeDate': registrationDate.toLocaleString()
-        //     }
-        // }, (error, response, body) => {
-        //     if (error) { return console.log('ERRO: ' + error) }
-        //     if (response.statusCode == 200) {
-        //         console.log('\nMESSAGE: ' + body.message + '\n')
-
-        //         log('client/images/last...')
-        //         request.get(API_GATEWAY + '/images/last/?token=' + req.session.token, (error, result) => {
-        // if (error) { return console.log('ERROR: ' + error) }
-        // console.log('CODE: ' + result.statusCode)
-        // if (result.statusCode == 200) {
-        // console.log('BODY: ' + JSON.parse(result.body))
-        // images = JSON.parse(result.body)
-
-        // let imageId = mongoose.Types.ObjectId(images._id); Id atual
-        // let imageId = mongoose.Types.ObjectId(); // novo Id
-
         log('client/products/post...')
         request.post(API_GATEWAY + '/products?token=' + req.session.token, {
             json: {
@@ -426,10 +409,6 @@ module.exports = () => {
                 })
             }
         })
-        //         }
-        //     })
-        // }
-        // })
     })
 
     /* LIST PRODUCTS */
@@ -455,8 +434,8 @@ module.exports = () => {
     /* PRODUCT */
     router.get('/produto/:id', (req, res, next) => {
         log('client/product/SKU...')
-        let search = { values: { sku: req.params.id }, fields: 'sku', ordination: 1, limit: 1 }
-        request.get(API_GATEWAY + '/product/' + JSON.stringify(search), (error, result) => {
+        let findProduct = { values: { sku: req.params.id }, fields: 'sku', ordination: 1, limit: 1 }
+        request.get(API_GATEWAY + '/product/' + JSON.stringify(findProduct), (error, result) => {
             if (error) { return console.log('ERROR: ' + error) }
             // console.log('CODE: ' + result.statusCode)
             if (result.statusCode == 200) {
