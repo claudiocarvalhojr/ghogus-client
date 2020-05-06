12f6f2c346e6e98a2c556d82e5ef7a147a61725c
ghogus <claudio.omar.jr@gmail.com>
2020-05-05 23:29:49 -0300

    merge do cart entre session e login

--- passport/signup.js
+++ passport/signup.js
@@ -1,6 +1,6 @@
-let LocalStrategy = require('passport-local').Strategy;
-let User = require('../models/user');
-let bCrypt = require('bcrypt-nodejs');
+let LocalStrategy = require('passport-local').Strategy
+let User = require('../models/user')
+let bCrypt = require('bcrypt-nodejs')
 
 module.exports = (passport) => {
     passport.use('signup', new LocalStrategy({
@@ -12,37 +12,37 @@ module.exports = (passport) => {
             findOrCreateUser = () => {
                 User.findOne({ 'email': email }, (err, user) => {
                     if (err) {
-                        return done(err, req.flash('message', 'Erro ao tentar criar o usuário!'));
+                        return done(err, req.flash('message', 'Erro ao tentar criar o usuário!'))
                     }
                     if (user) {
-                        return done(null, false, req.flash('message', 'Este e-mail já esta sendo utilizado!'));
+                        return done(null, false, req.flash('message', 'Este e-mail já esta sendo utilizado!'))
                     } else {
-                        let newUser = new User();
+                        let newUser = new User()
                         let registrationDate = new Date()
-                        newUser.email = email;
-                        newUser.password = createHash(password);
-                        newUser.birthDate = req.body.birthDate;
-                        newUser.phoneNumber = req.body.phoneNumber;
-                        newUser.firstName = req.body.firstName;
-                        newUser.lastName = req.body.lastName;
-                        newUser.registrationDate = registrationDate.toLocaleString();
-                        newUser.changeDate = registrationDate.toLocaleString();
-                        // newUser.cpfCnpj = null;
-                        // newUser.typePerson = null;
-                        // newUser.adresses = null;
+                        newUser.email = email
+                        newUser.password = createHash(password)
+                        newUser.birthDate = req.body.birthDate
+                        newUser.phoneNumber = req.body.phoneNumber
+                        newUser.firstName = req.body.firstName
+                        newUser.lastName = req.body.lastName
+                        newUser.registrationDate = registrationDate.toLocaleString()
+                        newUser.changeDate = registrationDate.toLocaleString()
+                        // newUser.cpfCnpj = null
+                        // newUser.typePerson = null
+                        // newUser.adresses = null
                         newUser.save((err) => {
                             if (err) {
-                                throw err;
+                                throw err
                             }
-                            return done(null, newUser, req.flash('message', 'Usuário cadastrado com sucesso!'));
-                        });
+                            return done(null, newUser, req.flash('message', 'Usuário cadastrado com sucesso!'))
+                        })
                     }
-                });
-            };
-            process.nextTick(findOrCreateUser);
+                })
+            }
+            process.nextTick(findOrCreateUser)
         })
-    );
+    )
     let createHash = (password) => {
-        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null)
     }
 }
